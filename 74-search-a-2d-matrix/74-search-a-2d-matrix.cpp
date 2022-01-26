5c773fb4da74375e9fc1996b575f03669f9201ff
Abhinav Kumar <72845356+abhinav-8@users.noreply.github.com>
2022-01-26 21:30:00 +0530

    Time: 16 ms (6.46%), Space: 9.4 MB (78.56%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
-    int get_row(vector<vector<int>>& matrix,int target){
-        int low=0,high=matrix.size()-1;
-        int ans=-1;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int col=matrix[0].size(),row=matrix.size(),low=0,high=row*col-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(matrix[mid][0]<=target){
+            if(matrix[mid/col][mid%col]==target)
+                return true;
+            if(matrix[mid/col][mid%col]<target)
                 low=mid+1;
-                ans=mid;
-            }
-            else{
+            else 
                 high=mid-1;
-            }
-        }
-        return ans;
-    }
-    bool search_target(vector<vector<int>>& matrix, int target,int row){
-        int low=0,high=matrix[0].size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(matrix[row][mid]==target) return true;
-            if(matrix[row][mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
         }
         return false;
-    }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row=get_row(matrix,target);
-        if(row==-1) return false;
-        return search_target(matrix,target,row);
-        
     }
 };
